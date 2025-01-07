2e7345f9d9b33f349d68b6db840ab799c36c6a3b
trantu0711 <dh52107408@student.stu.edu.vn>
2025-01-07 15:15:11 +0700

    hoan thanh cau 2

--- dethi.sh
+++ dethi.sh
@@ -2,7 +2,21 @@
 
 echo "Nhap so nguyen: "
 read n
-echo "So vua nhap la %d",n
-#Cau tren chua dung. Hay tao ra 1 nhanh moi co ten la fixbug de sua lai cho dung 
+echo "So vua nhap la %d, $n ";
+#Cau tren chua dung. Hay tao ra 1 nhanh moi co ten la  fixbug  de sua lai cho dung 
 #va lam them yeu cau ben duoi 
 #Viet tiep de kiem tra xem n co phai la so chinh phuong khong
+
+
+i =0 
+while [$i -le $n]; do
+    if ($i % 2 -eq $n ); then
+    echo "khong phai la so chinh phuong"
+    breck;
+    else 
+    (($i++))
+    fi
+    echo "la so chinh phuong"
+done
+
+
