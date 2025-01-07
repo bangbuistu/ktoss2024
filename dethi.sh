0df2967ba6aab9cc52faf5ecd7f9461e8e72cf7d
Dat <dh52110786@student.stu.edu.vn>
2025-01-07 14:46:17 +0700

    fixbug

--- dethi.sh
+++ dethi.sh
@@ -2,7 +2,24 @@
 
 echo "Nhap so nguyen: "
 read n
-echo "So vua nhap la %d",n
+sum=0
+sum1=1
+echo "So vua nhap la: $n"
+if((n%2==0));
+then
+
+	for((i=1;i<=n;i++))
+	do
+		sum=$((sum+i))
+	done
+	echo "Tong= $sum"
+else 
+	for((i=1;i<=n;i++))
+	do
+		sum1=$((sum1*i))
+	done
+	echo "Tich= $sum1"
+fi
 #Cau tren chua dung. Hay tao ra 1 nhanh moi co ten la fixbug de sua lai cho dung 
 #va lam them yeu cau ben duoi 
 #Viet tiep chuong trinh theo yeu cau sau
