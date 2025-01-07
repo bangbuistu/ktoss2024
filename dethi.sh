0ffaf8c55d123e32ad8b6f71be99ab9f3d0eb696
Bang <bang.buinhat@stu.edu.vn>
2025-01-07 09:10:51 +0700

    nhanh15

--- dethi.sh
+++ dethi.sh
@@ -5,4 +5,7 @@ read n
 echo "So vua nhap la %d",n
 #Cau tren chua dung. Hay tao ra 1 nhanh moi co ten la fixbug de sua lai cho dung 
 #va lam them yeu cau ben duoi 
-#Viet tiep de kiem tra xem n co phai la so chinh phuong khong
+#Viet tiep chuong trinh theo yeu cau sau
+#Kiem tra n la so chan thi tinh tong cac so tu 1 den n. 
+#Neu n la so le thi tinh tich cac so tu 1 den n.
+#In ra ket qua.
